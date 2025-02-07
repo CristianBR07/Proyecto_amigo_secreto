dbb98fbf52d955d97550761e654d2a7e5c64c315
Cristian Barrios <cristianbarrios3018@gmail.com>
2025-02-06 21:50:33 -0500

    Agregación de todas las funciones del backlog

--- app.js
+++ app.js
@@ -1,2 +1,40 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-adfad
+let amigos = [];
+
+function agregarAmigo() {
+    let nombre = document.getElementById('amigo').value.trim();
+    if (nombre === '') {
+        alert('Por favor, inserte un nombre.');
+        return;
+    }
+    amigos.push(nombre);
+    document.getElementById('amigo').value = '';
+    actualizarLista();
+}
+
+
+function actualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    // Recorrer el array amigos y agregar cada nombre a la lista
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos en la lista. Agrega al menos un nombre.");
+        return;
+    }
+
+    let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+
+    // Mostrar el resultado en la lista de resultados
+    document.getElementById("resultado").innerHTML = amigoSorteado;
+}
+
+
+
